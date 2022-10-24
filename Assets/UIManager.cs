47b9e3cd1acf480698e1138f38ea28294c0ecab7
LAPTOP-7H4OOR82\Danielle <danielle.s@lls-ltd.com>
2022-10-24 10:45:04 +0300

    adaptable ui wip

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public CanvasScaler canvasScalar;
     private void OnEnable()
     {
-        if (Tools.IsMobile())
+        if (!Tools.IsMobile())
         {
-            //TODO: fix UI
+            enabled = false;
         }
+        canvasScalar.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        canvasScalar.matchWidthOrHeight = 0.38f;
+        //TODO: if portrair -> OnPortrait() else -> OnLandscape()
+    }
+
+
+    void OnLandscape()
+    { 
+    }
+
+    void OnPortrait()
+    { 
     }
 
  
