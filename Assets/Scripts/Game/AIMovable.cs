c07a4070807b514f3fc684a688e7252af02d32df
LAPTOP-7H4OOR82\Danielle <danielle.s@lls-ltd.com>
2022-11-26 16:57:58 +0200

    no message

--- Assets/Scripts/Game/AIMovable.cs
+++ Assets/Scripts/Game/AIMovable.cs
@@ -6,7 +6,7 @@ public class AIMovable : RigidBodyMovable
 {
 
     public float threshHoldPuckDistance = 30f;
-    public float maxSpeed = 15f;
+    public float maxSpeed = 13f;
 
     private Vector3 InitPos;
     Vector3 targetPosition = new Vector3();
